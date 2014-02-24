d30c4d18efc964ce0443d594fdbdaffe474082a4
Huntout Zhang <huntout@gmail.com>
2014-02-24 15:51:12 +0800

    directive spec

--- directive/templates/spec.js
+++ directive/templates/spec.js
@@ -15,9 +15,9 @@ describe('<%= camelname %>', function() {
 
     /*
     To test your directive, you need to create some html that would use your directive,
-    send that through compile() then compare the results.
+    send that through $compile() then compare the results.
 
-    var element = compile('<div mydirective name="name">hi</div>')(scope);
+    var element = $compile('<div mydirective>hi</div>')($scope);
     expect(element.text()).toBe('hello, world');
     */
   });
